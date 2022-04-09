0985ab57a034c139eec41db4671e823c5dab4e79
Audhil <smdaudhilbe@gmail.com>
2022-04-09 13:34:16 +0530

    initial commit

---
+++ src/main/java/com/medium/audhil/CompleteSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.medium.audhil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompleteSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompleteSpringBootAppApplication.class, args);
+	}
+
+}
